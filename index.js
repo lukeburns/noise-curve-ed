4b7236b5d6c0de7465e1e324f111a3a214499678
Luke Burns <lukemburns@gmail.com>
2025-12-09 15:10:49 -0800

    red25519

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /* eslint-disable camelcase */
-const sodium = require('sodium-universal')
+const red25519 = require('red25519')
 const assert = require('nanoassert')
-const b4a = require('b4a')
 
-const DHLEN = sodium.crypto_scalarmult_ed25519_BYTES
-const PKLEN = sodium.crypto_scalarmult_ed25519_BYTES
-const SCALARLEN = sodium.crypto_scalarmult_ed25519_BYTES
-const SKLEN = sodium.crypto_sign_SECRETKEYBYTES
-const ALG = 'Ed25519'
+const DHLEN = 32
+const PKLEN = 32 // ed25519 public keys are 32 bytes
+const SCALARLEN = 32
+const SKLEN = 64 // red25519 secretKey is 64 bytes (32-byte private + 32-byte public)
+const ALG = 'Red25519'
 
 module.exports = {
   DHLEN,
@@ -17,55 +16,30 @@ module.exports = {
   ALG,
   name: ALG,
   generateKeyPair,
+  generateSeedKeyPair,
   dh
 }
 
-function generateKeyPair (privKey) {
-  if (privKey) return generateSeedKeyPair(privKey.subarray(0, 32))
-
-  const keyPair = {}
-  keyPair.secretKey = b4a.alloc(SKLEN)
-  keyPair.publicKey = b4a.alloc(PKLEN)
-
-  sodium.crypto_sign_keypair(keyPair.publicKey, keyPair.secretKey)
-  return keyPair
+function generateKeyPair (seed) {
+  if (seed) return red25519.keyPair(seed.subarray(0, 32))
+  return red25519.keyPair()
 }
 
 function generateSeedKeyPair (seed) {
-  const keyPair = {}
-  keyPair.secretKey = b4a.alloc(SKLEN)
-  keyPair.publicKey = b4a.alloc(PKLEN)
-
-  sodium.crypto_sign_seed_keypair(keyPair.publicKey, keyPair.secretKey, seed)
-  return keyPair
+  assert(seed.byteLength === SCALARLEN, 'seed must be 32 bytes')
+  return generateKeyPair(seed)
 }
 
 function dh (publicKey, { scalar, secretKey }) {
-  // tweaked keys expose scalar directly
-  if (!scalar) {
-    assert(secretKey.byteLength === SKLEN)
+  assert(publicKey.byteLength === PKLEN, `publicKey must be ${PKLEN} bytes`)
 
-    // libsodium stores seed not actual scalar
-    const sk = b4a.alloc(64)
-    sodium.crypto_hash_sha512(sk, secretKey.subarray(0, 32))
-    sk[0] &= 248
-    sk[31] &= 127
-    sk[31] |= 64
-
-    scalar = sk.subarray(0, 32)
+  if (scalar) {
+    assert(scalar.byteLength === SCALARLEN, 'scalar must be 32 bytes')
+    const localKeyPair = red25519.deriveKeyPair(scalar)
+    return red25519.deriveSharedSecret(localKeyPair.secretKey, publicKey)
   }
 
-  assert(scalar.byteLength === SCALARLEN)
-  assert(publicKey.byteLength === PKLEN)
+  assert(secretKey.byteLength === SKLEN, 'secretKey must be 64 bytes')
 
-  const output = b4a.alloc(DHLEN)
-
-  // we clamp if necessary above
-  sodium.crypto_scalarmult_ed25519_noclamp(
-    output,
-    scalar,
-    publicKey
-  )
-
-  return output
+  return red25519.deriveSharedSecret(secretKey, publicKey)
 }
